feab2e862ff1a8e133ffc2e442396d0c9a0dda5b
S401-07 <S401-07@S401-07>
2016-08-17 15:36:56 +0900

    20160817 자바스크립트 실습 (구구단, 별피라미드)

---
+++ wp_ch6/WebContent/hello.js
@@ -0,0 +1,8 @@
+/* window.location.href="http://www.sungkyul.ac.kr"; */
+alert("Hello World!");
+document.write( "<h1>Hello World</h1>" ); 
+
+console.log("hello world!");
+console.error("It's Hello World Error");
+console.info("It's Hello World Info");
+console.warn("It's Hello World Warnning")
